6594ab239f325054732ae216b9374a3c0dbdc95e
Latif siddiq sunny <lsiddiqsunny@gmail.com>
2020-09-06 05:49:22 +0600

    Updated

--- Number Theory/Sum of divisors.cpp
+++ Number Theory/Sum of divisors.cpp
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MOD 1e9 + 7
+
+typedef pair<int, int> pii;
+#define x first
+#define y second
+vector<int>primes;// pre-calculated prime
+pii extendedEuclid(int a, int b)
+{ // returns x, y | ax + by = gcd(a,b)
+    if (b == 0)
+        return pii(1, 0);
+    else
+    {
+        pii d = extendedEuclid(b, a % b);
+        return pii(d.y, d.x - d.y * (a / b));
+    }
+}
+
+int modularInverse(int a, int n)
+{
+    pii ret = extendedEuclid(a, n);
+    return ((ret.x % n) + n) % n;
+}
 int sod(long long int n)
 {
     long long sum = 1;
     long long nowsum = 0;
     long long rt = sqrtl(n);
-    for (int i = 0; primes[i] <= rt; i++) // pre-calculated prime
+    for (int i = 0; primes[i] <= rt; i++) 
     {
         if (n % primes[i] == 0)
         { //cout<<primes[i]<<endl;
